788b5c03ac95bce29be4b7bb141026490f9a325e
Bohdan <103610285+Bodya350@users.noreply.github.com>
2022-10-31 17:36:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
  document.querySelector(".menu-btn").onclick = () => {
  document.querySelector("body").classList.toggle("lock");
  }
+
+ 
+document.querySelector('.scroll_top').onclick = () => {
+   document.documentElement.scrollTop = 0;
+}
