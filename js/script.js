63546d40948af928b8500d5c2c9722ada0c1d1fc
Bohdan <103610285+Bodya350@users.noreply.github.com>
2022-10-31 18:02:31 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
- document.querySelector(".menu-btn").onclick = () => {
- document.querySelector("body").classList.toggle("lock");
- }
-
- 
-document.querySelector('.scroll_top').onclick = () => {
-   document.documentElement.scrollTop = 0;
-}
